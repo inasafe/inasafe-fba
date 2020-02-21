295868d81b5adf916ac3b6f9ec103c33335c8c7b
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-02-21 19:33:09 +0700

    Reorganize files

--- fixtures/schema/02_fbf/01_materialized_views/00_administrative_mapping.sql
+++ fixtures/schema/02_fbf/01_materialized_views/00_administrative_mapping.sql
@@ -2,14 +2,30 @@
 -- Name: mv_administrative_mapping; Type: MATERIALIZED VIEW; Schema: public; Owner: -
 --
 DROP MATERIALIZED VIEW IF EXISTS public.mv_administrative_mapping cascade;
-CREATE  MATERIALIZED VIEW public.mv_administrative_mapping AS
- SELECT district.dc_code AS district_id,
-    district.name AS district_name,
-    sub_district.sub_dc_code AS sub_district_id,
-    sub_district.name AS sub_district_name,
-    village.village_code AS village_id,
-    village.name AS village_name
-   FROM ((public.district
-     JOIN public.sub_district ON ((district.dc_code = (sub_district.dc_code)::double precision)))
-     JOIN public.village ON (((sub_district.sub_dc_code)::double precision = village.sub_dc_code)))
-  WITH NO DATA;
+CREATE MATERIALIZED VIEW public.mv_administrative_mapping AS
+select a.district_id,
+       a.district_name,
+       a.sub_district_id,
+       a.sub_district_name,
+       a.village_id,
+       a.village_name
+from (SELECT district.dc_code                         AS district_id,
+             district.name                            AS district_name,
+             sub_district.sub_dc_code                 AS sub_district_id,
+             sub_district.name                        AS sub_district_name,
+             village.village_code                     AS village_id,
+             village.name                             AS village_name,
+             row_number() over (partition by
+                 district.name, sub_district.name, village.name
+                 order by village.village_code DESC ) as row_number
+      FROM ((public.district
+          JOIN public.sub_district ON ((district.dc_code =
+                                        (sub_district.dc_code)::double precision)))
+               JOIN public.village
+                    ON (((sub_district.sub_dc_code)::double precision =
+                         village.sub_dc_code)))) a
+where a.row_number = 1
+    WITH NO DATA;
+
+create unique index if not exists mv_administrative_mapping_uindex
+    on mv_administrative_mapping(district_name, sub_district_name, village_name)
--- fixtures/schema/03_external_module/00_tables/00_census_kemendagri.sql
+++ fixtures/schema/03_external_module/00_tables/00_census_kemendagri.sql
@@ -1,42 +1,42 @@
 -- Table structure taken from ArcGIS REST API of GIS Dukcapil Kemendagri Indonesia:
 -- source: https://gis.dukcapil.kemendagri.go.id/peta/
 -- ArcGIS REST API Endpoint: https://gis.dukcapil.kemendagri.go.id/arcgis/rest/services/Data_Baru_26092017/MapServer/
-
-create table if not exists public.census_kemendagri
+create table census_kemendagri
 (
-    objectid bigint not null ,
-    no_prop float8,
-    no_kab float8,
-    no_kec float8,
-    no_kel float8,
+    objectid bigint not null,
+    no_prop double precision,
+    no_kab double precision,
+    no_kec double precision,
+    no_kel double precision,
     kode_desa_ varchar(25),
     nama_prop_ varchar(40),
-    nama_kab_s varchar(40),
-    nama_kec_s varchar(40),
-    nama_kel_s varchar(40),
-    jumlah_pen float8,
-    jumlah_kk float8,
-    pria float8,
-    wanita float8,
-    u0 float8,
-    u5 float8,
-    u10 float8,
-    u15 float8,
-    u20 float8,
-    u25 float8,
-    u30 float8,
-    u35 float8,
-    u40 float8,
-    u45 float8,
-    u50 float8,
-    u55 float8,
-    u60 float8,
-    u65 float8,
-    u70 float8,
-    u75 float8,
-    p01_belum_ float8,
-    constraint census_kemendagri_pk primary key (objectid)
+    nama_kab_s varchar(40) not null,
+    nama_kec_s varchar(40) not null,
+    nama_kel_s varchar(40) not null,
+    jumlah_pen double precision,
+    jumlah_kk double precision,
+    pria double precision,
+    wanita double precision,
+    u0 double precision,
+    u5 double precision,
+    u10 double precision,
+    u15 double precision,
+    u20 double precision,
+    u25 double precision,
+    u30 double precision,
+    u35 double precision,
+    u40 double precision,
+    u45 double precision,
+    u50 double precision,
+    u55 double precision,
+    u60 double precision,
+    u65 double precision,
+    u70 double precision,
+    u75 double precision,
+    p01_belum_ double precision,
+    constraint census_kemendagri_pk
+        primary key (nama_kab_s, nama_kec_s, nama_kel_s)
 );
 
-create unique index if not exists census_kemendagri_objectid_uindex
+create unique index census_kemendagri_objectid_uindex
     on census_kemendagri (objectid);
