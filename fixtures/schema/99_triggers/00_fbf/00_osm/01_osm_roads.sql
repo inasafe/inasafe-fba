32de370d7b23b4fc53f2361467b83bf0dad30f2a
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-02-25 01:13:50 +0700

    Add Road Type Mapper functions

--- fixtures/schema/99_triggers/00_fbf/00_osm/01_osm_roads.sql
+++ fixtures/schema/99_triggers/00_fbf/00_osm/01_osm_roads.sql
@@ -1,2 +1,5 @@
+DROP TRIGGER IF EXISTS osm_roads_tg_00_road_type_mapper_tg ON public.osm_roads;
+CREATE TRIGGER osm_roads_tg_00_road_type_mapper_tg BEFORE INSERT OR UPDATE ON public.osm_roads FOR EACH ROW EXECUTE PROCEDURE public.kartoza_road_type_mapping();
+
 DROP TRIGGER IF EXISTS osm_roads_tg_00_building_admin_mapper_tg ON public.osm_roads;
 CREATE TRIGGER osm_roads_tg_00_building_admin_mapper_tg BEFORE INSERT OR UPDATE ON public.osm_roads FOR EACH ROW EXECUTE PROCEDURE public.kartoza_road_admin_boundary_mapper();
