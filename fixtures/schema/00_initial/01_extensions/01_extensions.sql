edc7e6a76a51dcaf10fee26942255e4ef677f721
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-04-30 17:30:18 +0700

    Disable postgis_raster extension until switching to postgis 3

--- fixtures/schema/00_initial/01_extensions/01_extensions.sql
+++ fixtures/schema/00_initial/01_extensions/01_extensions.sql
@@ -1,4 +1,6 @@
 create extension if not exists postgis;
-create extension if not exists postgis_raster;
+-- Only applicable in postgis 3
+-- TODO: switch this on when in postgis 3
+-- create extension if not exists postgis_raster;
 create extension if not exists pg_cron;
 create extension if not exists plpython3u;
