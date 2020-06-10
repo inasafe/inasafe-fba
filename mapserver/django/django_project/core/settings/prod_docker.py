b52a9cff37c4a2f8c0c03d0151f5ff71e38a8ea0
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-06-10 10:26:21 +0700

    Refactor backend orchestration
    (cherry picked from commit fc427aa850e10b0763b04d32eb2d4ad5dca8986c)

--- mapserver/django/django_project/core/settings/prod_docker.py
+++ mapserver/django/django_project/core/settings/prod_docker.py
@@ -9,12 +9,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USERNAME'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': os.environ['DATABASE_HOST'],
-        'PORT': 5432,
-        'TEST_NAME': 'unittests',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(ABS_PATH('./'), 'db.sqlite3'),
     }
 }
